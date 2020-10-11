1ac3cccc736b53a5406311a839c58141cbdf46cc
Christoph Hermanns <christoph.jan.alexander.hermanns@rwth-aachen.de>
2020-10-11 17:03:47 +0200

    Projektdateien hinzufügen.

---
+++ Desktop_App/WorkEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Desktop_App
+{
+    public class WorkEntry
+    {
+        public DateTime Begin { get; set; }
+        public DateTime End { get; set; }
+        public TimeSpan Break { get; set; }
+    }
+}
---
+++ Desktop_App/WorkVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data;
+
+namespace Desktop_App
+{
+    public class WorkVM
+    {
+        IDbConnection dbConnection;
+        public ObservableCollection<WorkEntry> DisplayEntries { get; set; }
+        public DateTime SelectedDate { get; set; } = DateTime.Now;
+
+
+        public void GetConnection()
+        {
+
+        }
+        public void LoadEntries()
+        {
+
+        }
+        public void ChangeEntries()
+        {
+
+        }
+        public void SaveEntries()
+        {
+
+        }
+    }
+}
